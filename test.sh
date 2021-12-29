d058a3ced07e3a69b82af84f26c788ec23f484f7
c9s <yoan@maicoin.com>
2021-12-29 13:54:00 +0800

    cat generated content only when command failed

--- test.sh
+++ test.sh
@@ -4,21 +4,18 @@ rm -f ./example/api/place_order_request_accessors.go
 
 go run ./cmd/requestgen -type PlaceOrderRequest -method GET -url "/api/v1/bullet" -debug \
         ./example/api && \
-            cat ./example/api/place_order_request_accessors.go && \
-            go test ./example/api
+            go test ./example/api || cat ./example/api/place_order_request_accessors.go
 
 go run ./cmd/requestgen -type PlaceOrderRequest -method GET -url "/api/v1/bullet" -debug \
         -responseType '"./example/api".Response' \
         -responseDataField Data \
         -responseDataType '"./example/api".Order' \
         ./example/api && \
-            cat ./example/api/place_order_request_accessors.go && \
-            go test ./example/api
+            go test ./example/api || cat ./example/api/place_order_request_accessors.go
 
 go run ./cmd/requestgen -type PlaceOrderRequest -method GET -url "/api/v1/bullet" -debug \
         -responseType 'interface{}' \
         ./example/api && \
-            cat ./example/api/place_order_request_accessors.go && \
-            go test ./example/api
+            go test ./example/api || cat ./example/api/place_order_request_accessors.go
 
 go run ./cmd/requestgen -type NoParamRequest -url /v1/bullet -method GET -debug ./example/api && go test ./example/api
