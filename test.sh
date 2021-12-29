8afd2700bf53855b81a37d2ba1fffb489126fa8a
c9s <yoan@maicoin.com>
2021-12-30 00:40:00 +0800

    rename files

--- test.sh
+++ test.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
-rm -f ./example/api/place_order_request_accessors.go
+rm -f ./example/api/place_order_request_requestgen.go
 
 go run ./cmd/requestgen -type PlaceOrderRequest -method GET -url "/api/v1/bullet" -debug \
         ./example/api && \
-            go test ./example/api || cat ./example/api/place_order_request_accessors.go
+            go test ./example/api || cat ./example/api/place_order_request_requestgen.go
 
 go run ./cmd/requestgen -type PlaceOrderRequest -method GET -url "/api/v1/bullet" -debug \
         -responseType '"./example/api".Response' \
         -responseDataField Data \
         -responseDataType '"./example/api".Order' \
         ./example/api && \
-            go test ./example/api || cat ./example/api/place_order_request_accessors.go
+            go test ./example/api || cat ./example/api/place_order_request_requestgen.go
 
 
 (cd example/api &&
@@ -20,11 +20,11 @@ go run ../../cmd/requestgen -type PlaceOrderRequest -method GET -url "/api/v1/bu
         -responseDataField Data \
         -responseDataType .Order \
         . && \
-            go test . || cat place_order_request_accessors.go)
+            go test . || cat place_order_request_requestgen.go)
 
 go run ./cmd/requestgen -type PlaceOrderRequest -method GET -url "/api/v1/bullet" -debug \
         -responseType 'interface{}' \
         ./example/api && \
-            go test ./example/api || cat ./example/api/place_order_request_accessors.go
+            go test ./example/api || cat ./example/api/place_order_request_requestgen.go
 
 go run ./cmd/requestgen -type NoParamRequest -url /v1/bullet -method GET -debug ./example/api && go test ./example/api
