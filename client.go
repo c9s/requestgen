3cec46ecfbaef538ffd8704b4bc4e9a1f2342bed
ycdesu <sychewo@gmail.com>
2021-12-29 17:53:26 +0800

    fix: add missing ctx

--- client.go
+++ client.go
@@ -1,19 +1,20 @@
 package requestgen
 
 import (
+	"context"
 	"net/http"
 	"net/url"
 )
 
 type AuthenticatedRequestBuilder interface {
 	// NewAuthenticatedRequest builds up the http request for authentication-required endpoints
-	NewAuthenticatedRequest(method, refURL string, params url.Values, payload interface{}) (*http.Request, error)
+	NewAuthenticatedRequest(ctx context.Context, method, refURL string, params url.Values, payload interface{}) (*http.Request, error)
 }
 
 // APIClient defines the request builder method and request method for the API service
 type APIClient interface {
 	// NewRequest builds up the http request for public endpoints
-	NewRequest(method, refURL string, params url.Values, payload interface{}) (*http.Request, error)
+	NewRequest(ctx context.Context, method, refURL string, params url.Values, payload interface{}) (*http.Request, error)
 
 	// SendRequest sends the request object to the api gateway
 	SendRequest(req *http.Request) (*Response, error)
