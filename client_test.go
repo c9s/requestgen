a12cfb3898e8cc9e417d4aeb2ec51326882aa077
c9s <yoanlin93@gmail.com>
2025-12-08 17:32:22 +0800

    call binance us api for test https://api.binance.us

--- client_test.go
+++ client_test.go
@@ -9,15 +9,15 @@ import (
 )
 
 func TestClient(t *testing.T) {
-	baseURL, err := url.Parse("https://api.binance.com")
+	baseURL, err := url.Parse("https://api.binance.us")
 	assert.NoError(t, err)
 
 	ctx := context.Background()
 	apiClient := &BaseAPIClient{
-		BaseURL:    baseURL,
+		BaseURL: baseURL,
 	}
 
-	req, err := apiClient.NewRequest(ctx, "GET", "/api/v3/ping", nil , nil)
+	req, err := apiClient.NewRequest(ctx, "GET", "/api/v3/ping", nil, nil)
 	if assert.NoError(t, err) {
 		assert.NotNil(t, req)
 
